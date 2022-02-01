815248a4eb177f918bb3db34eee1ca59c64c3767
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-31 23:22:41 -0800

    added swagger docs

--- app.js
+++ app.js
@@ -32,10 +32,16 @@ app.use(cors());
 app.use(xss());
 // extra packages
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 app.use("/api/v1/auth", authRouter);
