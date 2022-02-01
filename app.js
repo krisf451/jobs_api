08f7b6656319e36a53da7ee72570815dfa372355
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-31 20:47:50 -0800

    added security packages, getting ready for deployment

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+const xss = require("xss-clean");
 
 const authenticateUser = require("./middleware/authentication");
 
@@ -15,7 +19,17 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
