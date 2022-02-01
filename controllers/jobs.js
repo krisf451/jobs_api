c24bf94fe748a860c3b41808a8a44fb9c3dae059
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-31 19:11:05 -0800

    finished custom error handling

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,23 +4,40 @@ const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  if (!jobs) {
+    throw new NotFoundError("Invalid User");
+  }
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
+
 const getJob = async (req, res) => {
   const job = await Job.findOne({
     createdBy: req.user.userId,
     _id: req.params.id,
   });
+  if (!job) {
+    throw new NotFoundError(`No Job with that ID ${req.params.id}`);
+  }
   res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  if (company === "" || position === "" || !company || !position) {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
   const job = await Job.findOneAndUpdate(
-    { createdBy: req.user.userId, _id: req.params.id },
+    { createdBy: userId, _id: jobId },
     req.body,
     {
       new: true,
@@ -29,7 +46,7 @@ const updateJob = async (req, res) => {
     }
   );
   if (!job) {
-    throw new BadRequestError("Invalid Job ID");
+    throw new NotFoundError(`No Job with that ID ${req.params.id}`);
   }
   res.status(StatusCodes.OK).json({ job });
 };
@@ -39,7 +56,7 @@ const deleteJob = async (req, res) => {
     { useFindAndModify: false }
   );
   if (!job) {
-    throw new BadRequestError("Invalid Job ID");
+    throw new NotFoundError(`No Job with that ID ${req.params.id}`);
   }
   res.status(StatusCodes.OK).json({ status: "Successful Deletion", job: null });
 };
